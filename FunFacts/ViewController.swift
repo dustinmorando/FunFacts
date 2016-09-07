e6f856352dc9bef46ef1f96106b01a191c4f6995
Dustin Morando <dustinmorando@me.com>
2016-09-06 21:13:04 -0400

    added model file and 2 methods to the view controler to make facts appear in the the view when the button is pushed

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,19 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var funFactLabel: UILabel!
+    let factModel = FactModel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factModel.facts [0]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func showFunFact() {
+        funFactLabel.text = factModel.facts [1]
+    }
+    
 }
 
